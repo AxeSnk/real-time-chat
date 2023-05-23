dc01b3f4cc6fadc25fb31eab4119e6ac08102691
axesnk <alexanderaxenov@mail.ru>
2023-05-23 11:41:49 +0800

    Websocket

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import LongPulling from './LongPulling';
 import EventSourcing from './EventSourcing';
+import Websock from './Websock';
 import './app.css';
 
 const App = () => {
   return (
     <div>
-      <EventSourcing/>
+      <Websock/>
     </div>
   )
 };
