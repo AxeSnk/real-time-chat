6d4e6956aca46e112ccbc2f6bea33ade6cdb3aba
axesnk <alexanderaxenov@mail.ru>
2023-05-23 10:41:50 +0800

    Init. Longpulling

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import LongPulling from './LongPulling';
+import './app.css';
+
+const App = () => {
+  return (
+    <div>
+      <LongPulling/>
+    </div>
+  )
+};
+
+export default App;
